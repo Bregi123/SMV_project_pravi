2eaea739e653699a2379e028216d6ec84a6a9b86
Bregi123 <144102219+Bregi123@users.noreply.github.com>
2023-09-19 11:09:08 +0200

    ja

--- Login.php
+++ Login.php
@@ -57,7 +57,7 @@ if (isset($_POST['submit'])) {
         <div style="margin-top: 100px; text-align: center;" >
     <form>
         <div class="row mb-3">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
           <div class="col-sm-10">
             <input type="email" class="form-control" id="inputEmail3">
           </div>
@@ -68,7 +68,7 @@ if (isset($_POST['submit'])) {
             <input type="password" class="form-control" id="inputPassword3">
           </div>
         </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" class="btn btn-primary">Log in</button>
       </form>
 </div>
 
--- info.php
+++ info.php
@@ -12,11 +12,41 @@
         <div class="dropdown">
             <button class="dropbtn">Menu</button>
             <div class="dropdown-content">
-                <a href="Login.html">Login</a>
+                <a href="Login.php">Login</a>
                 <a href="Home_page.php">Home</a>
 
             </div>
-            <h1>INFO</h1>
+            <div style="margin-top: 100px;">
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+            </div>
 
 
 </body>
