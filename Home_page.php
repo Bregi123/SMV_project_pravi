acbd262555e8ef75739222491d2bdd8b80d2bab8
Luka1321 <luka.bombek123@gmail.com>
2023-09-19 08:40:32 +0200

    baje

--- Home_page.php
+++ Home_page.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" href="Css.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 
 </head>
-<body class = Homepage_background>
-    <div class="">
-    <div class="container1">
-    
-    <div class="dropdown">
-        <button class="dropbtn">MENU</button>
-        <div class="dropdown-content">
-          <a href="Login.php">Login</a>
-          <a href="info.html">Info</a>
-        </div>
-      
-    </div>
-</div>
-    <div class="t"><h1 >HOME</h1></div>
-</div >
-<img src="Learning.png" class="rounded float-end" alt="...">
-</div>
 
-    
+<body class=Homepage_background>
+    <div class="">
+        <div class="container1">
+
+            <div class="dropdown">
+                <button class="dropbtn">MENU</button>
+                <div class="dropdown-content">
+                    <a href="Login.php">Login</a>
+                    <a href="info.html">Info</a>
+                </div>
+
+            </div>
+        </div>
+        <div class="t">
+            <h1>HOME</h1>
+        </div>
+    </div>
+    <img src="Learning.png" class="rounded float-end" alt="...">
+    </div>
+
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
 </html>
